3a23ec6c8e41b39f298cc4d4fc4e0ef6040ae8a8
Rongkai Wang <nhzx118wrk@163.com>
2022-07-25 19:33:52 +0800

    添加项目文件。

---
+++ Project3rd/3rd.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main() {
+//	int a = 10;
+//	int b = a++;
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main() {
+//	int a = 1;
+//	int b = (++a) + (++a) + (++a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+int main() {
+	int include = 0;
+	int define = 0;
+	int def = 0;
+	return 0;
+}
